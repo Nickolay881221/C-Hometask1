63c338292f9be76b12adcd8ba43658bc03ec956c
Nick Volodin <Volod-nick@mail.ru>
2022-10-03 23:25:52 +0300

    fourth task done

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+int Promt(string message)
+{
+    System.Console.Write(message);
+    string inputString = Console.ReadLine();
+    int value = Convert.ToInt32(inputString);
+    return value;
+}
+
+int Number = Promt("Введите число >");
+int Count = 2;
+while (Count <= Number)
+{
+    System.Console.Write($"{Count} ");
+    Count = Count + 2;
+}
