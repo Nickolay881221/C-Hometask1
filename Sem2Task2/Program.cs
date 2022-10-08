48dceb7adee5be07c155c4426d27fd73253932d8
Nick Volodin <Volod-nick@mail.ru>
2022-10-08 10:47:53 +0300

    Task 2 Sem 2 Finished

---
+++ Sem2Task2/Program.cs
@@ -0,0 +1,5 @@
+﻿//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа. Не использовать строки для расчета.
+
+int number1 = new Random().Next(99,1000);
+System.Console.WriteLine(number1);
+System.Console.WriteLine(number1/100*10+number1%10);
