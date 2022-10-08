63c91e5ef4497e91fc3831c40cefe1e3b55f6dc3
Nick Volodin <Volod-nick@mail.ru>
2022-10-08 10:34:14 +0300

    2 task 2 sem added

---
+++ Sem2Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Не использовать строки для расчета.
+
+int Promt(string message)
+{
+    System.Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int number = Promt("Введите трехначное число");
+System.Console.WriteLine(number%100/10);
