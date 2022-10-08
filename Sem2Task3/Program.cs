b2ec446215de00c7d808c44d18fcb613ccacebd4
Nick Volodin <Volod-nick@mail.ru>
2022-10-08 10:55:21 +0300

    3 Task 2 sem Finished

---
+++ Sem2Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. Не использовать строки для расчета.
+
+int Promt(string message)
+{
+    System.Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int number = Promt("Введите число");
+
+if (number / 100 == 0)
+{
+    System.Console.WriteLine("Третья цифра отсутствует");
+}
+else
+{
+    System.Console.WriteLine(number / 100 % 10);
+}
