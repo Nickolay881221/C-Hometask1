c8b7e36bc291417c09330609ebd8fcdee9d341b1
Nick Volodin <Volod-nick@mail.ru>
2022-10-15 14:52:22 +0300

    Seminar 3 Task 3 done

---
+++ Seminar3/Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int Promt(string message)
+{
+    System.Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int num = Promt("Введите число >");
+System.Console.WriteLine("");
+int count = 1;
+while (count <= num)
+{
+    System.Console.Write($"{count*count*count} ");
+    count = count +1;
+}
