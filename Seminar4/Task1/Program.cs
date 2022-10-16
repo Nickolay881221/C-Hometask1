2d39d473e21a99f6cfad1dc94278a117f9c90398
Nick Volodin <Volod-nick@mail.ru>
2022-10-16 22:05:35 +0300

    Seminar 4 Task 1_ multi_finished

---
+++ Seminar4/Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B (и в нулевую степень). Использовать свои функции, не использовать Math.Pow
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+int Promt(string message)
+{
+    System.Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int a = Promt("Введите первое число >");
+int b = Promt("Введите второе число >");
+int multi = 1;
+
+for (int i = 0; i <= b; i++)
+{
+    if (i == 0)
+    {
+        multi = 1;
+    }
+    else
+    {
+        if (i == 1)
+        {
+            multi = a;
+        }
+        else
+        {
+            multi = multi * a;
+        }
+    }
+}
+
+System.Console.WriteLine(multi);
