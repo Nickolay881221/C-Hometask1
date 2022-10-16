5da7539794f9261d1f6e2cd3fc29e127ffed0093
Nick Volodin <Volod-nick@mail.ru>
2022-10-16 22:28:54 +0300

    Sem4 Task3 RandomNumbers

---
+++ Seminar4/Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая задаёт массив из 8 случайных элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 7, 8
+
+int[] RandomArray()
+
+    {
+        int[] NewArray = new int[8];
+        for (int i = 0; i < NewArray.Length; i++)
+        {
+            NewArray[i] = new Random().Next(-999,999);
+        }
+        
+        
+        for (int j = 0; j < NewArray.Length; j++)
+        {
+            System.Console.Write($"{NewArray[j]} ");
+        }
+
+        return NewArray;
+    }
+
+RandomArray();
